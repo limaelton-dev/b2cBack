87f2d2043b88ebcc4cfb7b87b9829a9a4deb909f
JohnisonF <john-ison@hotmail.com>
2024-09-24 09:35:47 -0300

    Get dados de um produto

--- src/controllers/produto/produto.controller.ts
+++ src/controllers/produto/produto.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Produto } from 'src/models/produto/produto';
 import { ProdutoService } from 'src/services/produto/produto.service';
 
 @Controller('produtos')
 export class ProdutoController {
-  constructor(private readonly produtoService: ProdutoService) {}
+    constructor(private readonly produtoService: ProdutoService) {}
 
-  @Get()
-  async buscarProdutos(@Query('s') s: string) {
-    return this.produtoService.buscarProduto(s);
-  }
+    @Get(':id')
+    async getProductByCodigo(@Param('id') id: number): Promise<Produto> {
+        return this.produtoService.getProduto(id);
+    }
+
+    @Get()
+    async buscarProdutos(@Query('s') s: string): Promise<Produto[]> {
+        return this.produtoService.buscarProduto(s);
+    }
 }
--- src/services/produto/produto.service.ts
+++ src/services/produto/produto.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ILike, Repository } from 'typeorm';
 import { Produto } from 'src/models/produto/produto';
@@ -9,6 +9,16 @@ export class ProdutoService {
         @InjectRepository(Produto)
         private produtoRepository: Repository<Produto>,
     ) {}
+
+    async getProduto(id: number): Promise<Produto> {
+        const product = this.produtoRepository.findOne({where: {pro_codigo: id}});
+        
+        if (!product) {
+            throw new NotFoundException(`Produto com id '${id}' não encontrado`);
+        }
+
+        return product;
+    }
 
     async buscarProduto(s: string): Promise<Produto[]> {
         return this.produtoRepository.find({
