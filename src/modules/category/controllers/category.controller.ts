335a70a118808424460fb57e160d51837ab2bb32
JohnisonF <john-ison@hotmail.com>
2025-05-13 22:35:37 -0300

    backend products page

--- src/modules/category/controllers/category.controller.ts
+++ src/modules/category/controllers/category.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { CategoryService } from '../services/category.service';
 
 @Controller('category')
@@ -9,5 +9,10 @@ export class CategoryController {
     @Get('menu')
     async getCategoryMenu() {
       return this.categoryService.getCategoryMenu();
+    }
+
+    @Get('products')
+    async getProdutosTipoLimit(@Query('limit') limit: number) {
+      return this.categoryService.getProdutosTipoLimit(limit);
     }
 }
